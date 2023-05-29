fa8e6d6281047c63ce547d2fd055224a4863b432
jhonatasfender <jhonatas.fender@gmail.com>
2023-05-29 17:24:44 -0300

    feat: implemeting the first step the of connection with database

---
+++ packages/career-sync-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'career-sync-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/career-sync-api',
+};
