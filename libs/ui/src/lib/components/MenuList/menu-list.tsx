03f11d7d52d5150f262752e5119e86403c15491f
jhonatasfender <jhonatas.fender@gmail.com>
2024-06-15 20:46:48 -0300

    chore: ajustando o menu para reconher sub rotas

--- libs/ui/src/lib/components/MenuList/menu-list.tsx
+++ libs/ui/src/lib/components/MenuList/menu-list.tsx
@@ -1,26 +1,10 @@
-import { useReducer } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-
 import MenuItem from './menu-item';
-import { menuReducer } from './menu-list-reducer';
+import useMenu from './menu-list-reducer';
 import { MenuListProps } from './menu-list.structure';
 import { MenuListWrapper } from './menu-list.styles';
 
 const MenuList = ({ items }: MenuListProps) => {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const initialState = items.map((item) => ({
-    ...item,
-    active: item.path === location.pathname,
-  }));
-
-  const [menuItems, dispatch] = useReducer(menuReducer, initialState);
-
-  const handleItemClick = (path: string) => {
-    dispatch({ type: 'SET_ACTIVE', payload: path });
-    navigate(path);
-  };
+  const { menuItems, handleItemClick } = useMenu(items);
 
   return (
     <MenuListWrapper>
