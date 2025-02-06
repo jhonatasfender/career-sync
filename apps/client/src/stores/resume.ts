dcd93433c44e1701a9387a354f30ffee614141e8
guuiij <guilherme.jadson@hotmail.com>
2025-02-06 16:39:56 -0300

    feat(resume): add persist middleware for state persistence

--- apps/client/src/stores/resume.ts
+++ apps/client/src/stores/resume.ts
@@ -8,7 +8,7 @@ import _set from "lodash.set";
 import type { TemporalState } from "zundo";
 import { temporal } from "zundo";
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { useStoreWithEqualityFn } from "zustand/traditional";
 
@@ -26,56 +26,62 @@ type ResumeStore = {
 };
 
 export const useResumeStore = create<ResumeStore>()(
-  temporal(
-    immer((set) => ({
-      resume: {} as ResumeDto,
-      setValue: (path, value) => {
-        set((state) => {
-          if (path === "visibility") {
-            state.resume.visibility = value as "public" | "private";
-          } else {
-            state.resume.data = _set(state.resume.data, path, value);
-          }
-
-          void debouncedUpdateResume(JSON.parse(JSON.stringify(state.resume)));
-        });
-      },
-      addSection: () => {
-        const section: CustomSectionGroup = {
-          ...defaultSection,
-          id: createId(),
-          name: t`Custom Section`,
-          items: [],
-        };
-
-        set((state) => {
-          const lastPageIndex = state.resume.data.metadata.layout.length - 1;
-          state.resume.data.metadata.layout[lastPageIndex][0].push(`custom.${section.id}`);
-          state.resume.data = _set(state.resume.data, `sections.custom.${section.id}`, section);
-
-          void debouncedUpdateResume(JSON.parse(JSON.stringify(state.resume)));
-        });
-      },
-      removeSection: (sectionId: SectionKey) => {
-        if (sectionId.startsWith("custom.")) {
-          const id = sectionId.split("custom.")[1];
-
+  persist(
+    temporal(
+      immer((set) => ({
+        resume: {} as ResumeDto,
+        setValue: (path, value) => {
           set((state) => {
-            removeItemInLayout(sectionId, state.resume.data.metadata.layout);
-            // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-            delete state.resume.data.sections.custom[id];
+            if (path === "visibility") {
+              state.resume.visibility = value as "public" | "private";
+            } else {
+              state.resume.data = _set(state.resume.data, path, value);
+            }
 
             void debouncedUpdateResume(JSON.parse(JSON.stringify(state.resume)));
           });
-        }
-      },
-    })),
+        },
+        addSection: () => {
+          const section: CustomSectionGroup = {
+            ...defaultSection,
+            id: createId(),
+            name: t`Custom Section`,
+            items: [],
+          };
+
+          set((state) => {
+            const lastPageIndex = state.resume.data.metadata.layout.length - 1;
+            state.resume.data.metadata.layout[lastPageIndex][0].push(`custom.${section.id}`);
+            state.resume.data = _set(state.resume.data, `sections.custom.${section.id}`, section);
+
+            void debouncedUpdateResume(JSON.parse(JSON.stringify(state.resume)));
+          });
+        },
+        removeSection: (sectionId: SectionKey) => {
+          if (sectionId.startsWith("custom.")) {
+            const id = sectionId.split("custom.")[1];
+
+            set((state) => {
+              removeItemInLayout(sectionId, state.resume.data.metadata.layout);
+              // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+              delete state.resume.data.sections.custom[id];
+
+              void debouncedUpdateResume(JSON.parse(JSON.stringify(state.resume)));
+            });
+          }
+        },
+      })),
+      {
+        limit: 100,
+        wrapTemporal: (fn) => devtools(fn),
+        partialize: ({ resume }) => ({ resume }),
+      }
+    ),
     {
-      limit: 100,
-      wrapTemporal: (fn) => devtools(fn),
-      partialize: ({ resume }) => ({ resume }),
-    },
-  ),
+      name: "resume-storage", // Nome do localStorage
+      getStorage: () => localStorage, // Define onde armazenar os dados
+    }
+  )
 );
 
 export const useTemporalResumeStore = <T>(
